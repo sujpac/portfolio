2588e4c310eaa17d1b1a47ffea9d7202b16dff53
sujpac <sujit.pack@gmail.com>
2021-04-12 03:13:06 -0700

    Add title and link fields to the Job model

--- jobs/models.py
+++ jobs/models.py
@@ -3,7 +3,9 @@ from django.db import models
 
 class Job(models.Model):
     image = models.ImageField(upload_to='images/')
+    title = models.CharField(max_length=50, default='Job Title')
     summary = models.CharField(max_length=200)
+    link = models.URLField(blank=True)
 
     def __str__(self):
         return self.summary[:10]
